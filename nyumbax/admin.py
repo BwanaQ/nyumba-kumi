dd5fd7eecfec5df59c87cc6cf3ad1f29d4edaec6
BwanaQ <thunjawax@gmail.com>
2021-04-11 11:32:47 +0300

    add models to hood app

--- nyumbax/admin.py
+++ nyumbax/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hood, Business, Post, Essential, Location
 
 # Register your models here.
+admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Essential)
+admin.site.register(Location)
--- nyumbax/models.py
+++ nyumbax/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Location(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Hood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Business(models.Model):
+    title = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Essential(models.Model):
+    title = models.CharField(max_length=100)
+    officer = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
