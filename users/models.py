09581582834c40d8d7d0949e45334f7e9e9c0b3b
BwanaQ <thunjawax@gmail.com>
2021-04-12 07:45:10 +0300

    create full crud logic for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from nyumbax.models import Hood
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hood = models.ForeignKey(Hood, on_delete=models.CASCADE)
+    bio = models.TextField()
+    image = models.ImageField(default='default_fiis58.jpg',
+                              upload_to='nyumbax_profile_pics')
+    phone_number = PhoneField(
+        blank=True, help_text='Contact phone number', E164_only=False)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
